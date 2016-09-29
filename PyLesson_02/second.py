ad8d7e869b0ca17a278995c61191d66ad5afef52
wuk7579 <w38373063u@gmail.com>
2016-09-29 09:47:34 -0700

    fixed Lab_02

--- PyLesson_02/second.py
+++ PyLesson_02/second.py
@@ -1,3 +1,4 @@
+
 name = "Kevin Wu"
 city = "San Diego"
 zipcode = 92130
@@ -6,6 +7,6 @@ address = "11365 E San Raphael"
 
 print (name)
 print (address)
-print (city, ",", state, zipcode)
-
+print (city,",", state, zipcode)
+print ("Address Line 3")
 

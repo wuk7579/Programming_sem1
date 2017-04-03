5c0158e1329b85e46816a6c4a49e105b5b020a05
wuk7579 <w38373063u@gmail.com>
2017-04-02 20:31:34 -0700

    weblesson_14

---
+++ Semester 2/Weblesson_14/weblesson_14.js
@@ -0,0 +1,11 @@
+function validate() {
+    var x = document.forms.input.username.value;
+    var atpos = x.indexOf("@");
+    var dotpos = x.lastIndexOf(".");
+    if(atpos < 1 || dotpos < atpos+2 || dotpos + 2 > x.length)
+        alert("We were unable to process your request."" Please correct the following errors..."
+            <br/> "You did not provide a valid email address "
+            <br/>"Your password does not meet the minimum requirements");
+    else
+        alert("success");
+}

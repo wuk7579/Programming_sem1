37395d044a5a724e5eb2f58f79f342b80b3f2c39
wuk7579 <w38373063u@gmail.com>
2017-04-04 20:51:21 -0700

    weblesson_14

---
+++ Semester 2/Weblesson_14/scripts.js
@@ -0,0 +1,8 @@
+function validate() {
+    var x = document.forms.input.username.value;
+    var atpos = x.indexOf("@");
+    var
+
+    if(atpos < 1 || )
+        alter("This is not a valid email address!!!!")
+}

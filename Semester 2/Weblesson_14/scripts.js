6f8f86638d95e8bcaeaf3d6383d9a0be84b4d3e7
wuk7579 <w38373063u@gmail.com>
2017-04-05 10:24:45 -0700

    1

--- Semester 2/Weblesson_14/scripts.js
+++ Semester 2/Weblesson_14/scripts.js
@@ -1,10 +1,14 @@
 function validate() {
     var x = document.forms.input.username.value;
+    var y = document.forms.input.password.value;
+    var passnum = y
     var atpos = x.indexOf("@");
     var dotpos = x.lastIndexOf(".")
 
     if(atpos < 1 || dotpos < atpos+2 || atpos + 2 >x.length)
         alert("This is not a valid email address!!!!")
+    if(passnum < 6)
+        alert("Your password does not meet the minimum requirements")
     else
         alert("success!!")
 }

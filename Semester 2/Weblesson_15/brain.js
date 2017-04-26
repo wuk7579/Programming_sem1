447ea42f6d2f9a4a8b87e5b65e3405760a02dc69
wuk7579 <w38373063u@gmail.com>
2017-04-26 09:53:56 -0700

    finished weblesson_15

--- Semester 2/Weblesson_15/brain.js
+++ Semester 2/Weblesson_15/brain.js
@@ -1,11 +1,31 @@
-function shapes(){
+function shapes()
+{
     var x = document.getElementById("canvas");
     canvas = x.getContext("2d");
-    var g = canvas.createLinearGradient(10, 10 ,100, 200);
-    g.addColorStop(0, "blue";
-    g.addColorStop(1,"red");
-    
-    canvas.fillRect(10, 10, 100, 200);
+    var g = canvas.createLinearGradient(10, 10 ,700, 520);
+    g.addColorStop(0, "red");
+    g.addColorStop(0.3,"orange");
+    g.addColorStop(0.7, "yellow");
+    canvas.fillstyle = g;
+    canvas.beginPath();
+    canvas.moveTo(200,0);
+    canvas.lineTo(170 , 130);
+    canvas.lineTo(50 ,50 );
+    canvas.lineTo(130, 170);
+    canvas.lineTo(0, 200);
+    canvas.lineTo(130 ,230);
+    canvas.lineTo(50 ,350);
+    canvas.lineTo(170 , 270);
+    canvas.lineTo(200 , 400);
+    canvas.lineTo(230 , 270);
+    canvas.lineTo(350 , 350);
+    canvas.lineTo(270, 230);
+    canvas.lineTo(400 , 200);
+    canvas.lineTo(270 , 170);
+    canvas.lineTo(350 , 50);
+    canvas.lineTo(230, 130);
+    canvas.closePath();
+    canvas.fill();
 }
 
 window.addEventListener("load", shapes, false);

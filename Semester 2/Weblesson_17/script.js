e71ae5cf080a210806cda06cf2e5b4b45ce41cfb
wuk7579 <w38373063u@gmail.com>
2017-05-10 21:17:53 -0700

    17

--- Semester 2/Weblesson_17/script.js
+++ Semester 2/Weblesson_17/script.js
@@ -9,7 +9,7 @@ function drag() {
     leftbox.addEventListener("drop", drop, false);
 }
 function startDrag(e) {
-    var pic = '<img id = "pikachu" src = "https://www.gannett-cdn.com/-mm-/2f82a576ef5786ba3b027db0c17c35a5d71827f8/c=0-0-475-475&r=x203&c=200x200/local/-/media/2016/07/22/FortMyers/FortMyers/636047918655611038-STGBrd-07-18-2016-Spectrum-1-A001--2016-07-17-IMG-pikachu-1-1-B6F13IBP-L846611140-IMG-pikachu-1-1-B6F13IBP.jpg">'
+    var pic = '<img id = "pikachu" src = "https://www.gannett-cdn.com/-mm-/2f82a576ef5786ba3b027db0c17c35a5d71827f8/c=0-0-475-475&r=x203&c=200x200/local/-/media/2016/07/22/FortMyers/FortMyers/636047918655611038-STGBrd-07-18-2016-Spectrum-1-A001--2016-07-17-IMG-pikachu-1-1-B6F13IBP-L846611140-IMG-pikachu-1-1-B6F13IBP.jpg" >
     e.dataTransfer.setData('picture', pic);
 }
 function drop(e) {

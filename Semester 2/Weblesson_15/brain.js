e03dfce6d0a927a10b0cb54d7d826eb75fd76258
wuk7579 <w38373063u@gmail.com>
2017-05-30 10:12:40 -0700

    1

--- Semester 2/Weblesson_15/brain.js
+++ Semester 2/Weblesson_15/brain.js
@@ -1,31 +1,32 @@
-function shapes()
+function shapes ()
 {
     var x = document.getElementById("canvas");
     canvas = x.getContext("2d");
-    var g = canvas.createLinearGradient(10, 10 ,700, 520);
+    var g = canvas.createLinearGradient(0, 0, 400, 400);
     g.addColorStop(0, "red");
-    g.addColorStop(0.3,"orange");
-    g.addColorStop(0.7, "yellow");
-    canvas.fillstyle = g;
+    g.addColorStop(1, "blue");
+    canvas.fillStyle = g;
+    canvas.strokeStyle = "yellow";
     canvas.beginPath();
-    canvas.moveTo(200,0);
-    canvas.lineTo(170 , 130);
-    canvas.lineTo(50 ,50 );
-    canvas.lineTo(130, 170);
+    canvas.moveTo(200, 0);
+    canvas.lineTo(160, 100);
+    canvas.lineTo(60, 60);
+    canvas.lineTo(100, 160);
     canvas.lineTo(0, 200);
-    canvas.lineTo(130 ,230);
-    canvas.lineTo(50 ,350);
-    canvas.lineTo(170 , 270);
-    canvas.lineTo(200 , 400);
-    canvas.lineTo(230 , 270);
-    canvas.lineTo(350 , 350);
-    canvas.lineTo(270, 230);
-    canvas.lineTo(400 , 200);
-    canvas.lineTo(270 , 170);
-    canvas.lineTo(350 , 50);
-    canvas.lineTo(230, 130);
-    canvas.closePath();
+    canvas.lineTo(100, 240);
+    canvas.lineTo(60, 340);
+    canvas.lineTo(160, 300);
+    canvas.lineTo(200, 400);
+    canvas.lineTo(240, 300);
+    canvas.lineTo(340, 340);
+    canvas.lineTo(300, 240);
+    canvas.lineTo(400, 200);
+    canvas.lineTo(300, 160);
+    canvas.lineTo(340, 60);
+    canvas.lineTo(240, 100);
+    canvas.stroke();
     canvas.fill();
+    canvas.closePath();
 }
 
 window.addEventListener("load", shapes, false);

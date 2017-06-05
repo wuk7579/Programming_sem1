bf945615031bd307d0a47c3724acf56fd56061af
wuk7579 <w38373063u@gmail.com>
2017-06-05 16:05:13 -0700

    fix 14

--- Semester 2/Weblesson_14/scripts.js
+++ Semester 2/Weblesson_14/scripts.js
@@ -5,10 +5,15 @@ function validate() {
     var atpos = x.indexOf("@");
     var dotpos = x.lastIndexOf(".");
 
-    if(atpos < 1 || dotpos < atpos+2 || atpos + 2 >x.length)
-        alert("This is not a valid email address!!!!");
-    if(passnum.length < 6)
-        alert("Your password does not meet the minimum requirements");
-    else
-        alert("success!!");
+    if ((atpos < 1 || dotpos < atpos+2 || atpos + 2 >x.length) && passnum.length < 6)
+        alert("You do not have a valid email address and your passsword does not meetthe minimum requirements");
+
+    else{
+        if (atpos < 1 || dotpos < atpos+2 || atpos + 2 >x.length)
+            alert("This is not a valid email address!!!!");
+        else if (passnum.length < 6)
+            alert("Your password does not meet the minimum requirements");
+        else
+            alert("success!!");}
+
 }
